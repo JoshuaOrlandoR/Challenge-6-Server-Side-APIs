d0ea848f7b263a6a3d678db3da4c169018c52c1a
JoshuaOrlandoR <joshuaorlando.r@gmail.com>
2022-11-15 16:53:50 -0500

    Third Commit - added image folder, restructured html and edited styling because I missed the necessity of a 5 day forcast system

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+// Creating variable so API key is easily accessed 
+const apiKey = "726326cfb37bc9ce0441edd5bfd0266c"
+
+//Link for reference 
+// https://api.openweathermap.org/data/2.5/weather?q={city%20name}&appid={API%20key}&units=metric
